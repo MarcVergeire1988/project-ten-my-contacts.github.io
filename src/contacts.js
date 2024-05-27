c8e8645eddf986f149d5539d21aede79e84ca985
MarcVergeire1988 <vergeiremarcedison@gmail.com>
2024-05-27 19:21:55 -0400

    second commit

--- src/contacts.js
+++ src/contacts.js
@@ -1,27 +1,67 @@
 const contacts = [
     {
       id: 1,
-      name: "Beyonce",
+      name: "Elon Musk",
       imgURL:
-        "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+        "https://futureoflife.org/wp-content/uploads/2020/08/elon_musk_royal_society.jpg",
       phone: "+123 456 789",
-      email: "b@beyonce.com"
+      email: "elon@musk.com"
     },
     {
       id: 2,
-      name: "Jack Bauer",
+      name: "Mark Z",
       imgURL:
-        "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg",
       phone: "+987 654 321",
-      email: "jack@nowhere.com"
+      email: "mark@zuckerberg.com"
     },
     {
       id: 3,
-      name: "Chuck Norris",
+      name: "Bill Gates",
       imgURL:
-        "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+        "https://cdn.britannica.com/47/188747-050-1D34E743/Bill-Gates-2011.jpg",
       phone: "+918 372 574",
-      email: "gmail@chucknorris.com"
+      email: "bill@gates.com"
+    },
+    {
+      id: 4,
+      name: "Jeff Bezos",
+      imgURL:
+        "https://upload.wikimedia.org/wikipedia/commons/0/03/Jeff_Bezos_visits_LAAFB_SMC_%283908618%29_%28cropped%29.jpeg",
+      phone: "+917 372 574",
+      email: "jeff@bezos.com"
+    },
+    {
+      id: 5,
+      name: "Warren Buf",
+      imgURL:
+        "https://www.gatesfoundation.org/-/media/gfo/3about/3people/ga_warren_buffett_20210203_0001.jpg?rev=68ac5cb3b9db4b34aeb5e199666c3149",
+      phone: "+917 472 574",
+      email: "warren@buffett.com"
+    },
+    {
+      id: 6,
+      name: "Richard B",
+      imgURL:
+        "https://executivespeakers.com//Upload/Speaker/richard-branson.jpg",
+      phone: "+917 482 574",
+      email: "richard@branson.com"
+    },
+    {
+      id: 7,
+      name: "Ophrah W",
+      imgURL:
+        "https://hips.hearstapps.com/hmg-prod/images/oprah-winfrey-arrives-at-the-premiere-of-owns-david-makes-news-photo-1678107172.jpg",
+      phone: "+917 482 674",
+      email: "ophrah@winfrey.com"
+    },
+    {
+      id: 8,
+      name: "Taylor S",
+      imgURL:
+        "https://m.media-amazon.com/images/M/MV5BZGM0YjhkZmEtNGYxYy00OTk0LThlNDgtNGQzM2YwNjU0NDQzXkEyXkFqcGdeQXVyMTU3ODQxNDYz._V1_.jpg",
+      phone: "+917 482 684",
+      email: "taylor@swift.com"
     }
   ];
   
